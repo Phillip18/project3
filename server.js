7e785f11866cf24dec82d760aa9d7abaad20330f
filipp <filipp.krylovskiy@gmail.com>
2022-12-30 09:35:22 -0500

    Newest version

--- server.js
+++ server.js
@@ -2,24 +2,12 @@
 
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 const cors = require('cors');
-const { send } = require('express/lib/response');
 app.use(cors());
-app.get('/products/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-  })
-  /*app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'null');
-    next();
-  });*/ 
- 
 app.use(express.static('website'));
-
 const port = 3000;
 
 app.listen(port, function() {
@@ -27,18 +15,13 @@ app.listen(port, function() {
 });
 
 let projectData = {};
-//projectData = {...projectData, 'stupid': 'idiot'}
+projectData = {...projectData, 'stupid': 'idiot'}
 
-/*app.get('/', (request, response)=>{
-    console.log('sending...');
-    response.send('working on it');
-    response.send(a);
-});*/
 app.get('/', function (request, response) {
-    res.send({'Hurray!': 'happiness'});
     response.send(projectData);
   })
-app.post('post', (request, response)=>{
+app.post('', (request, response)=>{
+    console.log('REQUEST:')
     console.log(request);
     response.send('post received')
 })
--- website/app.js
+++ website/app.js
@@ -3,23 +3,20 @@ const apiKey = '8e204443b8f65beaafb4737d4e9f3dc0&units=imperial';
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip='
 const zip = '11204';
 const letters = ',us&appid='
-const heroku = 'https://cors-anywhere.herokuapp.com/'
-const url = heroku + baseUrl + zip + letters + apiKey;
+//const heroku = 'https://cors-anywhere.herokuapp.com/'
+const url = baseUrl + zip + letters + apiKey;
 const port = 3000;
 
 const getData = async () => {
-  console.log('https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid={API key}')
-  console.log(url)
-  const request = await fetch(url, {method: 'GET',});
+  const request = await fetch(url, { method: 'GET', });
   try {
     // Transform into JSON
     const allData = await request.json()
-    console.log(allData)
     // Write updated data to DOM elements
     document.getElementById('temp').innerHTML = Math.round(allData.temp) + 'degrees';
     document.getElementById('content').innerHTML = allData.feel;
     document.getElementById('date').innerHTML = allData.date;
-    console.log('done');
+    return allData;
   }
   catch (error) {
     console.log('error', error);
@@ -28,16 +25,35 @@ const getData = async () => {
   }
 }
 
-//const getDataFromServer = async (url = `http://localhost:${port}`, data = a) => {
+const postDataToServer = async (url = '', data) => {
+const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: { 'content-type': 'application.json', },
+    body: JSON.stringify("data")
+  });
+  try {
+    console.log(response.json());
+  }
+  catch(error) {
+    console.log(`Error posting data to the server:  ${error}`);
+  }
+}
+
 const getDataFromServer = async (url = `http://localhost:${port}`) => {
-  const response = await fetch(url, {method: 'GET',});
+console.log("You are a tripple cretin")
+const response = await fetch(url, { method: 'GET', });
   const data = await response.json();
-  console.log('done');  
-  console.log(data);
+  return data;
 };
 
 //document.getElementById('generate').addEventListener('click', () => {
-//  getData();
+getData().then(function(allData) {
+  postDataToServer(allData);
+});
+  //postDataToServer(allData)).then(display())
 //});
 
-getData();
+function display() {
+  console.log("You are a cretin")
+}
